3d928e1f65b00c0a2d434dd42de20fc01a2020e8
Syeda Sadia Mahfuz <sadia@Syedas-MacBook-Pro.local>
2018-08-25 12:54:49 +0100

    Added YouTube API key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
+
+const API_KEY = "AIzaSyD_zvnQfNkuDJHoxrY0Kqg1O4zhPaXjkEc";
 
 const App = function() {
   return <div> Hi! </div>;
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector(".container"))
