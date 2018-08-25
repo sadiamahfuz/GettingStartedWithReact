602bf643582a34598074a96179eca0aeb84fa4d9
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2018-08-25 21:45:06 +0100

    Convert App to be a class component and add state to hold list of videos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from "react"
+import React, {Component} from "react"
 import ReactDOM from "react-dom"
+import YTSearch from "youtube-api-search"
 
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyD_zvnQfNkuDJHoxrY0Kqg1O4zhPaXjkEc";
 
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+      this.setState({ videos });
+      //{videos} eq to {videos: videos} in ES6
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"))
