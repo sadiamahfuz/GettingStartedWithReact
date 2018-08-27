2b9db3d9e700619cd3d2e92985658ec4ee6db113
Sadia Mahfuz <mahfuzs@amazon.com>
2018-08-27 15:18:57 +0100

    add video image and title

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,16 +1,17 @@
 import React from "react"
 
-const VideoListItem = (props) => {
-	const video = props.video;
+const VideoListItem = ({video}) => {
+	const imageURL = video.snippet.thumbnails.default.url;
+	const title = video.snippet.title;
 
 	return (
 		<li className="list-group-item">
 			<div classname="video-list media">
 				<div className="media-left">
-					<img className="media-object" />
+					<img className="media-object" src={imageURL}/>
 				</div>
 				<div className="media-body">
-					<div className="media-heading" />
+					<div className="media-heading">{title}</div>
 				</div>
 			</div>
 		</li>
