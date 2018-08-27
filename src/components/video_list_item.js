e3848103442d413db2359708a502449b3865d2b2
Sadia Mahfuz <mahfuzs@amazon.com>
2018-08-27 14:35:42 +0100

    added bootstrap styling for video items

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,19 @@
 import React from "react"
 
 const VideoListItem = (props) => {
+	const video = props.video;
+
 	return (
-		<li>Video</li>
+		<li className="list-group-item">
+			<div classname="video-list media">
+				<div className="media-left">
+					<img className="media-object" />
+				</div>
+				<div className="media-body">
+					<div className="media-heading" />
+				</div>
+			</div>
+		</li>
 	);
 };
 
