2e124efc774af9f34f7dd0fbc14d58f2fc4a6961
Sadia Mahfuz <mahfuzs@amazon.com>
2018-08-27 17:02:03 +0100

    Add selected video to props

--- src/index.js
+++ src/index.js
@@ -12,11 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: "cricket"}, (videos) => {
-      this.setState({ videos });
-      //{videos} eq to {videos: videos} in ES6
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
     });
   }
 
@@ -24,7 +29,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
