5b94e3cfa6dbd78720c5d100d34e9833190a0a57
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2018-08-28 21:24:26 +0100

    fix typo in search bar styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,7 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div className="search_bar">
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={event => this.setState({ term: event.target.value })}
